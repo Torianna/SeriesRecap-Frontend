97fbfbf87470b57853d81660a770a60a734611ca
Klaudia <klaudia.goralska95@gmail.com>
2020-05-05 16:04:31 +0200

    Slider is done

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -10,23 +10,37 @@ import { SeriesService } from '../../services/series.service';
 export class MainComponent implements OnInit {
   series: Series[] = [];
   presentSeries: boolean = false;
-
-
+  seriesFormattedForCarousel : Series[][] = new Array();
 
   constructor(private seriesService: SeriesService) {
 
   }
 
+  alignSeriesAccordingToCarousel(){
+    var j = -1;
+    for (var i = 0; i < this.series.length; i++) {
+      if (i % 3 == 0) {
+        j++;
+        this.seriesFormattedForCarousel[j] = [];
+        this.seriesFormattedForCarousel[j].push(this.series[i]);
+      }
+      else {
+        this.seriesFormattedForCarousel[j].push(this.series[i]);
+      }
+    }
+
+  }
+
   ngOnInit() {
     this.getAllSeries();
-
-
   }
 
   async getAllSeries() {
     this.series = await this.seriesService.getAllSeries();
+
     console.log(this.series.length);
     this.presentSeries = true;
+    this.alignSeriesAccordingToCarousel();
   }
 }
 
